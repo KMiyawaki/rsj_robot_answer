9af2640187042e16366a7b7e76bb34e932a4dc5e
Kenzaburo Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2018-06-29 09:21:57 +0900

    セミナテキストの変更に追随

--- src/rsj_robot_test.cpp
+++ src/rsj_robot_test.cpp
@@ -4,45 +4,47 @@
 
 #include <tf/transform_datatypes.h>
 
-class rsj_robot_test_node
+class RsjRobotTestNode
 {
 private:
-    ros::Subscriber sub_odom;
-    ros::Publisher pub_twist;
+  ros::NodeHandle nh_;
 
-    void cb_odom(const nav_msgs::Odometry::ConstPtr &msg)
-    {
-    }
+  ros::Subscriber sub_odom_;
+  ros::Publisher pub_twist_;
+
+  void cbOdom(const nav_msgs::Odometry::ConstPtr &msg)
+  {
+  }
+
 public:
-    rsj_robot_test_node()
-    {
-        ros::NodeHandle nh("~");
-        pub_twist = nh.advertise<geometry_msgs::Twist>(
-                    "/ypspur_ros/cmd_vel", 5);
-        sub_odom = nh.subscribe("/ypspur_ros/odom", 5,
-                                &rsj_robot_test_node::cb_odom, this);
-    }
-    void mainloop()
-    {
-        ROS_INFO("Hello ROS World!");
+  RsjRobotTestNode()
+    : nh_()
+  {
+    pub_twist_ = nh_.advertise<geometry_msgs::Twist>(
+        "cmd_vel", 5);
+    sub_odom_ = nh_.subscribe(
+        "odom", 5, &RsjRobotTestNode::cbOdom, this);
+  }
+  void mainloop()
+  {
+    ROS_INFO("Hello ROS World!");
 
-        ros::Rate rate(10.0);
-        while(ros::ok())
-        {
-            ros::spinOnce();
-            // ここに速度指令の出力コード
-            rate.sleep();
-        }
-        // ここに終了処理のコード
+    ros::Rate rate(10.0);
+    while (ros::ok())
+    {
+      ros::spinOnce();
+      // ここに速度指令の出力コード
+      rate.sleep();
     }
+    // ここに終了処理のコード
+  }
 };
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "rsj_robot_test_node");
+  ros::init(argc, argv, "rsj_robot_test_node");
 
-    rsj_robot_test_node robot_test;
+  RsjRobotTestNode robot_test;
 
-    robot_test.mainloop();
+  robot_test.mainloop();
 }
-
